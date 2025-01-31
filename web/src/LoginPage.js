68eff1f68dc8f05733eb09c77fac41e133cfbac8
Ido Kapach <idommm10@gmail.com>
2025-01-31 15:02:15 +0200

    add login page structure

--- web/src/LoginPage.js
+++ web/src/LoginPage.js
@@ -1,8 +1,28 @@
 // render the login page
 function LoginPage({handleLogin}) {
-    return (
-        <div> LoginPage</div>
-    ) 
+  return (
+      <div class="d-flex align-items-center py-4 bg-body-tertiary" id="login-cart">
+  
+<main class="form-signin w-100 m-auto">
+<form>
+  <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+  <div class="form-floating">
+    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" fdprocessedid="k2b4s"/>
+    <label for="floatingInput">Username</label>
+  </div>
+  <div class="form-floating">
+    <input type="password" class="form-control" id="floatingPassword" placeholder="Password" fdprocessedid="6hoglo"/>
+    <label for="floatingPassword">Password</label>
+  </div>
+
+  <button class="btn btn-outline-primary w-100 py-2" type="submit" fdprocessedid="tv6em" style={{"margin-top": "8%"}}>Sign in</button>
+</form>
+</main>
+  
+
+<span id="PING_IFRAME_FORM_DETECTION" style={{display: null}}></span></div>
+  ) 
 }
 
 export default LoginPage
