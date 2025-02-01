228afa5fa7d43a6ee973ea526ed3b41b7949194d
Omri <omri.perry42@gmail.com>
2025-02-01 17:48:53 +0200

    added main view, User class, Glide library, app icon.

--- Android/app/src/main/java/com/example/targil4/MainActivity.java
+++ Android/app/src/main/java/com/example/targil4/MainActivity.java
@@ -7,8 +7,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.targil4.adapters.MovieAdapter;
+import com.example.targil4.entity.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView trendingRecycler;
+    private RecyclerView popularRecycler;
+    private RecyclerView recommendedRecycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,55 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // init recycler views
+        trendingRecycler = findViewById(R.id.trending_recycler);
+        popularRecycler = findViewById(R.id.popular_recycler);
+        recommendedRecycler = findViewById(R.id.recommended_recycler);
+
+        // get data to inflate recycler views (stump)
+        List<Movie> trendingMovies = getRandomMovies();
+        List<Movie> popularMovies = getRandomMovies();
+        List<Movie> recommendedMovies = getRecommendedMovies();
+
+        // create adapters
+        MovieAdapter trendingAdapter = new MovieAdapter(this, trendingMovies);
+        MovieAdapter popularAdapter = new MovieAdapter(this, popularMovies);
+        MovieAdapter recommendedAdapter = new MovieAdapter(this, recommendedMovies);
+
+        // set layout managers (horizontal)
+        trendingRecycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        popularRecycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        recommendedRecycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+        // set adapters to views
+        trendingRecycler.setAdapter(trendingAdapter);
+        popularRecycler.setAdapter(popularAdapter);
+        recommendedRecycler.setAdapter(recommendedAdapter);
+    }
+
+    private List<Movie> getRecommendedMovies() {
+        // stump - need to implement data fetching
+        // data based on recommend api route
+        List<Movie> movies = new ArrayList<>();
+        movies.add(new Movie("Movie 1", "https://via.placeholder.com/150"));
+        movies.add(new Movie("Movie 2", "https://via.placeholder.com/150"));
+        movies.add(new Movie("Movie 3", "https://via.placeholder.com/150"));
+        movies.add(new Movie("Movie 4", "https://via.placeholder.com/150"));
+        movies.add(new Movie("Movie 5", "https://via.placeholder.com/150"));
+        movies.add(new Movie("Movie 6", "https://via.placeholder.com/150"));
+        movies.add(new Movie("Movie 7", "https://via.placeholder.com/150"));
+
+        return movies;
+    }
+
+    private List<Movie> getRandomMovies() {
+        // stump - need to implement data fetching
+        // we don't need to actually use trending\popular - just random is good
+        List<Movie> movies = new ArrayList<>();
+        movies.add(new Movie("Movie 1", "https://via.placeholder.com/150"));
+        movies.add(new Movie("Movie 2", "https://via.placeholder.com/150"));
+        movies.add(new Movie("Movie 3", "https://via.placeholder.com/150"));
+        return movies;
     }
 }
